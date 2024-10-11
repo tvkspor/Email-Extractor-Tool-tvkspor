7135d8ab149d16ec99d3d9fed467a5e744e56e6c
tvkspor <tmtuan120704@gmail.com>
2024-10-11 08:39:25 +0700

    Update extract ips from email

--- mail-extractor.py
+++ mail-extractor.py
@@ -1,6 +1,7 @@
 import email
 import email.parser
 import sys
+import re
 
 def read_file(file_path):
     with open(file_path, 'rb') as file:
@@ -16,16 +17,30 @@ def main(file_path):
     print("To", msg['From'])
     print("Subject:", msg['Subject'])
     print("Date:", msg['Date'])
-    if msg.is_multipart():
-        for part in msg.iter_parts():
-            content_type = part.get_content_type()
-            content_disposition = part.get("Content-Disposition")
+    # if msg.is_multipart():
+    #     for part in msg.iter_parts():
+    #         content_type = part.get_content_type()
+    #         content_disposition = part.get("Content-Disposition")
 
-            if content_disposition is None:
-                if content_type == "text/plain":
-                    print("Body:", part.get_payload(decode=True).decode(part.get_content_charset()))
-    else:
-        print("Body:", msg.get_payload(decode=True).decode(msg.get_content_charset()))
+    #         if content_disposition is None:
+    #             if content_type == "text/plain":
+    #                 print("Body:", part.get_payload(decode=True).decode(part.get_content_charset()))
+    # else:
+    #     print("Body:", msg.get_payload(decode=True).decode(msg.get_content_charset()))
+
+def get_ip(email_message):
+    ip_set = set()    
+    for header_name, header_value in email_message.items():
+        ip_set.update(re.findall(r'\b(?:\d{1,3}\.){3}\d{1,3}\b'), header_value)
+    for part in email_message.walk():
+        content_type = part.get_content_type()
+        if content_type == 'text/plain' or content_type == 'text/html':
+            payload = part.get_payload(decode=True)
+            if isinstance(payload, bytes):
+                payload = payload.decode('utf-8', errors='ignore')
+            ip_set.update(re.findall(r'\b(?:\d{1,3}\.){3}\d{1,3}\b', payload))
+    
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
