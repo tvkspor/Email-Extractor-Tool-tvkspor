97d3eeede6b9755b9b7ffc38ec453c6701707dd6
tvkspor <tmtuan120704@gmail.com>
2024-10-12 10:47:18 +0700

    Update ip lookup

--- mail-extractor.py
+++ mail-extractor.py
@@ -4,6 +4,9 @@ import sys
 import re
 import ipaddress
 from email.header import decode_header 
+import hashlib
+import ipinfo
+import requests
 
 def read_file(file_path):
     with open(file_path, 'rb') as file:
@@ -36,6 +39,7 @@ def get_ip(email_message):
 
     for ip in ip_set:
         try: 
+            ip_lookup(ip)
             ipaddress.ip_address(ip)
             valid_ip_set.append(ip)
         except ValueError:
@@ -52,6 +56,84 @@ def get_url(email_message):
                 payload = payload.decode('utf-8', errors='ignore')
             url_set.update(re.findall(r'https?:\/\/(?:[\w\-]+\.)+[a-z]{2,}(?:\/[\w\-\.\/?%&=]*)?', payload))
     return list(url_set)
+
+def get_header(email_message):
+    headers_need=[
+        "Date",
+        "Subject",
+        "To",
+        "From",
+        "Reply-To",
+        "Return-Path",
+        "Message-ID",
+        "X-Originating-IP",
+        "X-Sender-IP",
+        "Authentication-Results"
+    ]
+    headers={}
+    for key in email_message.keys():
+        if key in headers_need:
+            headers[key]= email_message[key]
+    return headers
+
+def get_attachment(email_message):
+    attachments =[]
+    for part in email_message.walk():
+        if part.get_content_maintype() == 'multipart':
+            continue   
+        if part.get('Content-Disposition') is None:
+            continue
+        file_name = part.get_filename()
+        if file_name:
+            attachments.append({
+                'filename': file_name,
+                'md5': hashlib.md5(part.get_payload(decode=True)).hexdigest(),
+                'sha1': hashlib.sha1(part.get_payload(decode=True)).hexdigest(),
+                'sha256': hashlib.sha256(part.get_payload(decode=True)).hexdigest()
+            })
+
+    return attachments
+
+def check_reversed_ip(ip):
+    private_ranges = [
+        '10.0.0.0/8',
+        '172.16.0.0/12',
+        '192.168.0.0/16',
+    ]
+    reserved_ranges = [
+        '0.0.0.0/8',
+        '100.64.0.0/10',
+        '169.254.0.0/16',
+        '192.0.0.0/24',
+        '192.0.2.0/24',
+        '198.51.100.0/24',
+        '203.0.113.0/24',
+        '224.0.0.0/4',
+        '240.0.0.0/4',
+    ]
+    for r in private_ranges + reserved_ranges:
+        if ipaddress.ip_address(ip) in ipaddress.ip_network(r):
+            return True
+    return False
+
+def ipinfo(ip):
+    response = requests.get(f"https://ipinfo.io/{ip}/json")
+    if response.status_code != 200:
+        raise Exception(f"Failed to get IP info for {ip}")
+    return response.json()
+
+def ip_lookup(ip):
+    if check_reversed_ip(ip):
+        return None
+    
+    ip_info = ipinfo(ip)
+    print(f"City: {ip_info.get('city', 'N/A')}")
+    print(f"Location: {ip_info.get('loc', 'N/A')}")
+    print(f"Region: {ip_info.get('region', 'N/A')}")
+    print(f"Country: {ip_info.get('country', 'N/A')}")
+    print(f"Organization: {ip_info.get('org', 'N/A')}")
+    print(f"IP: {ip_info.get('ip', 'N/A')}")
+    return ip_info
 
 # Defang data for safer use
 def defangip(ip):
